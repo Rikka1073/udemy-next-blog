19890f6a83b51c069677ae1c14347815f5b13e93
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-01-22 16:43:43 +0900

    footer作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-2 px-4 border-t absolute bottom-0 w-full">
+      <small>@2025 Next Blog</small>
+    </footer>
+  );
+};
+
+export default Footer;
