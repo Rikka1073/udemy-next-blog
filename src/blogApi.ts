623a3f72f20bd3bffbd8131b406cdfafb02365f6
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-01-24 11:47:28 +0900

    詳細のAPI作成

--- src/blogApi.ts
+++ src/blogApi.ts
@@ -1,7 +1,9 @@
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" });
+
   if (!res.ok) {
     throw new Error("Error");
   }
@@ -10,4 +12,21 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticles = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, { next: { revalidate: 60 } });
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("Error");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const article = await res.json();
+  return article;
 };
