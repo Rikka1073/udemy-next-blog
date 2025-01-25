dc84e6714bc280f00ccbfe97e6abb2dadd2f9e6d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-01-25 22:21:40 +0900

    api作成

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,11 +1,14 @@
 import DeleteButton from "@/app/components/DeleteButton";
-import { getDetailArticles } from "@/blogApi";
+// import { getDetailArticles } from "@/blogApi";
 import Image from "next/image";
 import React from "react";
 
 const Article = async ({ params }: { params: { id: string } }) => {
-  const detailArticle = await getDetailArticles(params.id);
-  console.log(detailArticle);
+  // const detailArticle = await getDetailArticles(params.id);
+  // console.log(detailArticle);
+
+  const res = await fetch(`http://localhost:3000/api/${params.id}`, { next: { revalidate: 10 } });
+  const detailArticle = await res.json();
 
   return (
     <div className="max-w-3xl mx-auto p-5">
